f64fe7d02c602cb4966cad0d30f392bc4f456ee6
lanfurama <lanfurama@example.com>
2025-12-06 11:08:29 +0700

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { createServer as createViteServer } from 'vite';
-import routes from './api/routes/index.ts';
+import routes from './api/_routes/index.ts';
 
 dotenv.config();
 
